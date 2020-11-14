4688085999a037abebda96652f7760812a583e73
gabruw <gabrielmarques.mg@gmail.com>
2020-11-14 12:48:03 -0300

    feat: Criação do contexto

---
+++ Repository/Context/ProductContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Repository.Context
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
