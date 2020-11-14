c5ca03b79c1c6ec25a8379743a2bedcff3ca5bd3
gabruw <gabrielmarques.mg@gmail.com>
2020-11-14 14:15:45 -0300

    feat: Response

---
+++ Products/Utils/Respose.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Products.Utils
+{
+    public class Respose<TEntity>
+    {
+        public TEntity Data { get; set; }
+
+        private List<string> _errors;
+        public List<string> Errors
+        {
+            get
+            {
+                if (_errors == null)
+                {
+                    _errors = new List<string>();
+                }
+
+                return _errors;
+            }
+            set
+            {
+                _errors = value;
+            }
+        }
+
+        public Respose()
+        {
+
+        }
+
+        public bool hasErrors()
+        {
+            return Errors.Count() > 0;
+        }
+    }
+}
